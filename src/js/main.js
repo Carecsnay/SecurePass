747e65d7847291038e18242c644500b32e84b9d3
Bruno (Escritorio) <carecsnay1@gmail.com>
2023-07-06 16:30:18 -0300

    Feat: New action button.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 //É obrigatório setar a variável global para utilizar na função generatePassword e definir um valor para não dar problema no for da 
 //função, lembrando que 16 é o valor padrão do input.
 let passwordLength = 16;
+//Capturando elementos
+const inputPasswordEl = document.querySelector('#password');
+const inputRangeEl = document.querySelector('#password-length');
+const buttonEl = document.querySelector('button');
 
 const generatePassword = () => {
     const chars = "abcdefghijklmnopqrstuvwxyzçABCDEFGHIJKLMNOPQRSTUVWXYZÇ1234567890!@#$%&*()_+-[]{}"
@@ -10,23 +14,27 @@ const generatePassword = () => {
         //Numero randômico vai de 1 a comprimento de 'chars'.
         const randomNumber = Math.floor(Math.random() * chars.length)
         /*Após o calculo do resultado do sorteio randômico atribuímos o valor na variável password (que antes era ''). 
-        Agora utilizando o método substring faremos o recorte da posição randomNumber até a próxima posição (+1), o resultado será o recorte de 1 caractere 
-        sorteado que fará a composição da senha. */
-
+        Agora utilizando o método substring faremos o recorte da posição randomNumber até a próxima posição (+1), o resultado será o recorte de 1 caractere sorteado que fará a composição da senha. */
         password += chars.substring(randomNumber, randomNumber + 1)
-
     }
-    //Capturando input
-    const inputPasswordEl = document.querySelector('#password');
     //Setando senha randômica no campo password.
     inputPasswordEl.value = password;
 }
 
-//Capturando input de slider ou range, adicionando evento de escuta e pegando o seu valor.
-const inputRangeEl = document.querySelector('#password-length');
+const copy = () => {
+    navigator.clipboard.writeText(inputPasswordEl.value);
+}
+
+//Adicionando evento de escuta e pegando o seu valor.
 inputRangeEl.addEventListener('input', () => {
     passwordLength = inputRangeEl.value;
     generatePassword()
 })
 
+//Adicionando evento de escuta e acionando gatilho ao botão.
+buttonEl.addEventListener('click', () => {
+    generatePassword();
+})
+
 generatePassword()
+copy()
