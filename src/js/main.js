25bda29fe17c751b132772a9d20221537728f96f
Bruno (Escritorio) <carecsnay1@gmail.com>
2023-07-06 16:04:51 -0300

    Feat: Now password is created based on input range.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
-function generatePassword() {
-    const chars = "abcdefghijklmnopqrstuvwxyzçABCDEFGHIJKLMNOPQRSTUVWXYZÇ1234567890!@#$%&*()_+-[]{}"
+//É obrigatório setar a variável global para utilizar na função generatePassword e definir um valor para não dar problema no for da 
+//função, lembrando que 16 é o valor padrão do input.
+let passwordLength = 16;
 
+const generatePassword = () => {
+    const chars = "abcdefghijklmnopqrstuvwxyzçABCDEFGHIJKLMNOPQRSTUVWXYZÇ1234567890!@#$%&*()_+-[]{}"
     let password = ''
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < passwordLength; i++) {
         //Numero randômico vai de 1 a comprimento de 'chars'.
         const randomNumber = Math.floor(Math.random() * chars.length)
         /*Após o calculo do resultado do sorteio randômico atribuímos o valor na variável password (que antes era ''). 
@@ -17,10 +20,13 @@ function generatePassword() {
     const inputPasswordEl = document.querySelector('#password');
     //Setando senha randômica no campo password.
     inputPasswordEl.value = password;
-
-    //Capturando input de slider ou range
-    const inputRangeEl = document.querySelector('#password-length');
-
 }
+
+//Capturando input de slider ou range, adicionando evento de escuta e pegando o seu valor.
+const inputRangeEl = document.querySelector('#password-length');
+inputRangeEl.addEventListener('input', () => {
+    passwordLength = inputRangeEl.value;
+    generatePassword()
+})
 
 generatePassword()
